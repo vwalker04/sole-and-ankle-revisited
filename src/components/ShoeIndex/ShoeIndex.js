a4ecc99f17f75a7dcc8602797fb1965cee1dea03
Vaughn Walker <vwalk04@gmail.com>
2024-02-12 11:15:43 -0500

    Update shoe grid nav elements

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { WEIGHTS } from '../../constants';
+import {QUERIES, WEIGHTS} from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -9,63 +9,104 @@ import Spacer from '../Spacer';
 import ShoeSidebar from '../ShoeSidebar';
 import ShoeGrid from '../ShoeGrid';
 
-const ShoeIndex = ({ sortId, setSortId }) => {
-  return (
-    <Wrapper>
-      <MainColumn>
-        <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
-        </Header>
-        <Spacer size={32} />
-        <ShoeGrid />
-      </MainColumn>
-      <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
-        <Spacer size={42} />
-        <ShoeSidebar />
-      </LeftColumn>
-    </Wrapper>
-  );
+function ShoeBreadcrumb() {
+	return <Breadcrumbs>
+		<Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+		<Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+		<Breadcrumbs.Crumb href="/sale/shoes">
+			Shoes
+		</Breadcrumbs.Crumb>
+	</Breadcrumbs>;
+}
+
+const ShoeIndex = ({sortId, setSortId}) => {
+	return (
+		<Wrapper>
+			<MainColumn>
+				<Header>
+					<div>
+						<MobileBreadcrumbs>
+							<ShoeBreadcrumb/>
+						</MobileBreadcrumbs>
+						<Title>Running</Title>
+					</div>
+					<SelectWrapper>
+						<Select
+							label="Sort"
+							value={sortId}
+							onChange={(ev) => setSortId(ev.target.value)}
+						>
+							<option value="newest">Newest Releases</option>
+							<option value="price">Price</option>
+						</Select>
+					</SelectWrapper>
+				</Header>
+				<Spacer size={32}/>
+				<ShoeGrid/>
+			</MainColumn>
+			<LeftColumn>
+				<DesktopBreadcrumbs>
+					<ShoeBreadcrumb/>
+				</DesktopBreadcrumbs>
+				<Spacer size={42}/>
+				<ShoeSidebar/>
+			</LeftColumn>
+		</Wrapper>
+	);
 };
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: row-reverse;
-  align-items: baseline;
-  gap: 32px;
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: baseline;
+    gap: 32px;
 `;
 
+const DesktopBreadcrumbs = styled.div`
+    @media ${QUERIES.tabletAndSmaller} {
+        display: none;
+    }
+`
+
+const SelectWrapper = styled.div`
+    @media ${QUERIES.phoneAndSmaller} {
+        display: none;
+    }
+`
+
+const MobileBreadcrumbs = styled.div`
+    display: none;
+
+    @media ${QUERIES.tabletAndSmaller} {
+        display: flex;
+    }
+`
+
 const LeftColumn = styled.div`
-  flex-basis: 248px;
+    flex-basis: 248px;
+
+    @media ${QUERIES.tabletAndSmaller} {
+        display: none;
+    }
 `;
 
 const MainColumn = styled.div`
-  flex: 1;
+    flex: 1;
 `;
 
 const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: baseline;
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+
+    @media ${QUERIES.tabletAndSmaller} {
+        align-items: end;
+    }
 `;
 
 const Title = styled.h2`
-  font-size: 1.5rem;
-  font-weight: ${WEIGHTS.medium};
+    font-size: 1.5rem;
+    font-weight: ${WEIGHTS.medium};
 `;
 
 export default ShoeIndex;
